a057afb5c42d2849fadb8eef3a4a5ddf0cde48f4
quxianwei <quxw@goldencis.com>
2024-07-24 17:21:06 +0800

    整合GitHub

---
+++ src/main/java/online/k12code/oauth2/K12codeAuthorizationOauth2Application.java
@@ -0,0 +1,16 @@
+package online.k12code.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author msi
+ */
+@SpringBootApplication
+public class K12codeAuthorizationOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K12codeAuthorizationOauth2Application.class, args);
+    }
+
+}
